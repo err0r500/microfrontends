a626f63e606b4e8a05088a0b2112f80089eaf6fa
Matthieu Jacquot <matthieu.jacquot@schibsted.com>
2018-09-27 14:36:08 +0200

    remove react from package and load it once in index.html

--- components/react/webpack.config.js
+++ components/react/webpack.config.js
@@ -2,25 +2,29 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = env => {
-  return ({
-    entry: './src/hello-world.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'hello-world.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
+    return ({
+        entry: './src/hello-world.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'hello-world.js',
         },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
-      }),
-    ],
-  });
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+            ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+            }),
+        ],
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM"
+        }
+    });
 };
